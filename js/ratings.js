6a86302e373fd7bcc8f3946fe2d0b1c80e116453
tomwass <tom.f.wass@gmail.com>
2022-09-29 06:13:03 +1000

    Completed module

---
+++ js/ratings.js
@@ -0,0 +1,34 @@
+function collect_ratings(){
+    
+    let ratings = {
+        count: 0,
+        sum: 0,
+        average: 0,
+    }
+
+    let rating = 0;
+    
+    const elements = document.querySelectorAll('.rating')
+    //elements are input type with id=starx and value
+    elements.forEach(element => {
+        rating = parseInt(element.id.replace('star', ''));
+        console.log(`rating is ${rating}`)
+        ratings.count +=  parseInt(element.value);
+        ratings.sum += parseInt(element.value) * rating;
+    });
+
+    if (ratings.count !== 0) {
+        ratings.average = ratings.sum / ratings.count;
+    }
+
+    return ratings;
+}
+
+ 
+  document.addEventListener('change', (ev)=> {
+    const ratings = collect_ratings();
+
+    const el = document.getElementById('average');
+    console.log(`the element is ${el.value}`);
+    el.value = ratings.average.toFixed(2);
+  });
